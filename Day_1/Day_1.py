c667d4c52669cf214e14a156f3e4421d9231c44e
Tonka Ratkaj <58440835+TonkaRatkaj@users.noreply.github.com>
2019-12-02 20:50:18 +0100

    Add files via upload

---
+++ Day_1/Day_1.py
@@ -0,0 +1,14 @@
+# total fuel requirement
+
+def fuel_requirements(x):
+	fr = int(x/3) - 2
+	return fr
+
+f = open("in_day_1.txt")
+masses = f.readlines()
+suma = 0
+
+for mass in masses:
+	suma = suma + fuel_requirements(int(mass))
+
+print(suma)
